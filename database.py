88eb7dd6037dd19a1329a6d75b71ae80ee3bfe64
Sirapob PHASUT <omegaooh@gmail.com>
2023-11-18 14:06:23 +0700

    implement project_manage.py with its test code, add useful methods into the class in database.py

--- database.py
+++ database.py
@@ -25,6 +25,12 @@ class Database:
     def insert(self, table: "Table Class"):
         self.__tables.append(table)
 
+    def search(self, table_name: str):
+        for i in self.__tables:
+            if i.table_name == table_name:
+                return i
+        return None
+
     def __str__(self):
         output = ''
         for i in self.__tables:
@@ -33,7 +39,7 @@ class Database:
 
 
 class Table:
-    def __init__(self, table_name: str, data: list):
+    def __init__(self, table_name: str, data: list = []):
         self.__table_name = table_name
         self.__data = data
 
@@ -43,23 +49,30 @@ class Table:
 
     @property
     def data(self):
-        return self.data
+        return self.__data
 
     def insert_data(self, new_data):
         if isinstance(new_data, list):
             for i in new_data:
                 self.__data.append(i)
         else:
-            self.__data.append(i)
+            self.__data.append(new_data)
+
+    def search(self, key, search_query):
+        for i in self.__data:
+            if i[key] == search_query:
+                return i
+        return None
 
     def __str__(self):
         return f'{self.__table_name} : {self.__data}'
 
 
-# Test code
-test_table = Table('person', persons)
-test_table2 = Table('test', [{"1": 1, "2": 2, "3": 3}])
-my_db = Database()
-my_db.insert(test_table)
-my_db.insert(test_table2)
-print(my_db)
+# # Test code
+# test_table = Table('person', persons)
+# test_table2 = Table('test', [{"1": 1, "2": 2, "3": 3}])
+# my_db = Database()
+# my_db.insert(test_table)
+# my_db.insert(test_table2)
+# print(my_db)
+
--- project_manage.py
+++ project_manage.py
@@ -1,30 +1,53 @@
 # import database module
-
+import database
+import random
 # start by adding the admin related code
+main_db = database.Database()
 
 # create an object to read an input csv file, persons.csv
-
+persons = database.read_csv('persons.csv')
 # create a 'persons' table
-
+persons_table = database.Table('persons', persons)
 # add the 'persons' table into the database
-
+main_db.insert(persons_table)
 # create a 'login' table
-
 # the 'login' table has the following keys (attributes):
+login_table = database.Table('Login')
 
-# person_id
-# username
-# password
-# role
-
-# a person_id is the same as that in the 'persons' table
-# let a username be a person's fisrt name followed by a dot and the first letter of that person's last name
-# let a password be a random four digits string
-# let the initial role of all the students be Member
-# let the initial role of all the faculties be Faculty
-
-# you create a login table by performing a series of insert operations; each insert adds a dictionary to a list
+# add all persons into login Table with its information
+for i in persons_table.data:
+    login_info = {'person_id': i['ID'], 'username': i['fist'] + '.' + i['last'], 'password': '', 'role': ''}
+    # Generate Password
+    for _ in range(4):
+        login_info['password'] += str(random.randint(0, 9))
+    # Add role
+    if i['type'] == 'student':
+        login_info['role'] = 'Member'
+    elif i['type'] == 'faculty':
+        login_info['role'] = 'Faculty'
+    elif i['type'] == 'admin':
+        login_info['role'] = 'Admin'
+    # create a login table by performing a series of insert operations; each insert adds a dictionary to a list
+    login_table.insert_data(login_info)
 
 # add the 'login' table into the database
+main_db.insert(login_table)
 
-# add code that performs a login task; asking a user for a username and password; returning [person_id, role] if valid, otherwise returning None
+
+# code that performs a login task; asking a user for a username and password;
+# returning [person_id, role] if valid, otherwise returning None
+def login():
+    user = input('Please enter your username: ')
+    table = main_db.search('Login')
+    user_dict = table.search('username', user)
+    if user_dict is None:
+        print('user not found')
+        return None
+    pwd = input('Please enter your password: ')
+    if user_dict['password'] == pwd:
+        return user_dict['person_id'], user_dict['role']
+
+
+# Test Code
+print(main_db)
+print(login())
